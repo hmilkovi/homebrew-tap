0c5fa8682021f340935fb27fd17cb25cef7ab2e1
hmilkovi <hmilkovi@gmail.com>
2025-02-06 23:21:24 +0100

    tls-bench v0.1.2

--- Formula/tls-bench.rb
+++ Formula/tls-bench.rb
@@ -1,22 +1,22 @@
 class TlsBench < Formula
   desc "A TLS benchmarking tool for evaluating servers TLS handshake performance."
   homepage "https://github.com/hmilkovi/tls-bench"
-  version "0.1.1"
+  version "0.1.2"
 
   if OS.mac? && Hardware::CPU.intel?
     url ""
     url "https://github.com/hmilkovi/tls-bench/releases/download/v#{version}/tls-bench-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "5500274813647b1cadd5c1e2245fc6a5e3ee46ca63722179cb38d0a56fe42e35"
+    sha256 "dcf503fa42b524184882c13a6c813880de2a397a6e18c0a122498bfe88f5fad3"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/hmilkovi/tls-bench/releases/download/v#{version}/tls-bench-v#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "822cdff500bafc672a0b02ef36e7e0a4ae580dc3fd78d132334b7d5b46ead13b"
+    sha256 "c36aae4ab29dd22a2462a254f1dc509e72aaf2c6d805820ed1e43833006e725b"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/hmilkovi/tls-bench/releases/download/v#{version}/tls-bench-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "de5ce566b9f70fbe6a8e72de690c067d98bb66234c19c08c143828dbbf786d5d"
+    sha256 "ac5507043f2ed372db4bab30d5fa02291983625ff0de2c3dd2c811a377a46933"
   end
 
   def install
